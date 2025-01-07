716364ea867c4fc299f5536883cb33908f52af0e
koparasy <parasyris1@llnl.gov>
2025-01-06 16:57:07 -0800

    Fix libevent linkage, by side-stepping the issue

--- cmake/Findlibevent.cmake
+++ cmake/Findlibevent.cmake
@@ -6,16 +6,38 @@
 # LIBEVENT_FOUND - System has LibEvent
 # LIBEVENT_INCLUDE_DIR - the LibEvent include directory
 # LIBEVENT_LIBRARIES 0 The libraries needed to use LibEvent
-find_path     (LIBEVENT_INCLUDE_DIR NAMES event.h)
-find_library  (LIBEVENT_LIBRARY     NAMES event)
-find_library  (LIBEVENT_CORE        NAMES event_core)
-find_library  (LIBEVENT_EXTRA       NAMES event_extra)
+find_path     (LIBEVENT_INCLUDE_DIR NAMES event.h HINTS ${AMS_LIBEVENT_HINTS})
+find_library  (LIBEVENT_LIBRARY     NAMES event HINTS ${AMS_LIBEVENT_HINTS} NO_CMAKE_SYSTEM_PATH)
+
+if(NOT LIBEVENT_LIBRARY)
+    find_library(LIBEVENT_LIBRARY NAMES event)
+endif()
+
+find_library  (LIBEVENT_CORE NAMES event_core HINTS ${AMS_LIBEVENT_HINTS} NO_CMAKE_SYSTEM_PATH)
+if(NOT LIBEVENT_CORE)
+  find_library(LIBEVENT_CORE NAMES event_core)
+endif()
+
+
+find_library  (LIBEVENT_EXTRA       NAMES event_extra HINTS ${AMS_LIBEVENT_HINTS} NO_CMAKE_SYSTEM_PATH)
+if(NOT LIBEVENT_EXTRA)
+  find_library(LIBEVENT_EXTRA NAMES event_extra)
+endif()
+
 if (NOT EVHTP_DISABLE_EVTHR)
-    find_library (LIBEVENT_THREAD   NAMES event_pthreads)
+  find_library (LIBEVENT_THREAD NAMES event_pthreads HINTS ${AMS_LIBEVENT_HINTS} NO_CMAKE_SYSTEM_PATH)
+  if(NOT LIBEVENT_THREAD)
+    find_library(LIBEVENT_THREAD NAMES event_pthreads)
+  endif()
 endif()
+
 if (NOT EVHTP_DISABLE_SSL)
-    find_library (LIBEVENT_SSL      NAMES event_openssl)
+  find_library (LIBEVENT_SSL  NAMES event_openssl HINTS ${AMS_LIBEVENT_HINTS} NO_CMAKE_SYSTEM_PATH)
+  if(NOT LIBEVENT_SSL)
+    find_library(LIBEVENT_SSL NAMES event_openssl)
+  endif()
 endif()
+
 include (FindPackageHandleStandardArgs)
 set (LIBEVENT_INCLUDE_DIRS ${LIBEVENT_INCLUDE_DIR})
 set (LIBEVENT_LIBRARIES
