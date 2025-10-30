96da2c3e216002bd2dcc641ec2869ce36bdbd996
Loic Pottier <pottier1@llnl.gov>
2025-10-30 09:26:11 -0700

    Added Tuo and Tioga to the CI. Removed Lassen. Moved the tests to a matrix
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- scripts/gitlab/ci-build-test.sh
+++ scripts/gitlab/ci-build-test.sh
@@ -6,7 +6,6 @@ source scripts/gitlab/setup-env.sh
 export CTEST_OUTPUT_ON_FAILURE=1
 export NUMEXPR_NUM_THREADS=1
 export NUMEXPR_MAX_THREADS=1
-# WITH_CUDA is defined in the per machine job yml.
 
 cleanup() {
   if [ -n "$VIRTUAL_ENV" ]; then
@@ -17,16 +16,14 @@ cleanup() {
 }
 
 build_and_test() {
-  WITH_HDF5=${1}
-  WITH_MPI=${2}
-  WITH_WORKFLOW=${3}
 
   echo "*******************************************************************************************"
   echo "Build configuration" \
     "WITH_HDF5 ${WITH_HDF5}" \
     "WITH_MPI ${WITH_MPI}" \
     "WITH_WORKFLOW ${WITH_WORKFLOW}" \
-    "WITH_CUDA ${WITH_CUDA}"
+    "WITH_CUDA ${WITH_CUDA}" \
+    "WITH_HIP ${WITH_HIP}"
   echo "*******************************************************************************************"
 
   build_dir="/tmp/ams/$(uuidgen)"
@@ -44,12 +41,13 @@ build_and_test() {
     -DBUILD_SHARED_LIBS=On \
     -DCMAKE_PREFIX_PATH=$INSTALL_DIR \
     -DWITH_CALIPER=On \
-    -DWITH_HDF5=${WITH_HDF5} \
+    -DWITH_HDF5=${AMS_WITH_HDF5} \
     -DAMS_HDF5_DIR=$AMS_HDF5_PATH \
     -DCMAKE_INSTALL_PREFIX=./install \
     -DCMAKE_BUILD_TYPE=Release \
     -DCUDA_ARCH=$AMS_CUDA_ARCH \
     -DWITH_CUDA=${WITH_CUDA} \
+    -DWITH_HIP=${WITH_HIP} \
     -DWITH_MPI=${WITH_MPI} \
     -DWITH_TESTS=On \
     -DTorch_DIR=$AMS_TORCH_PATH \
@@ -67,10 +65,4 @@ build_and_test() {
   rm -rf ${build_dir}
 }
 
-# build_and_test WITH_HDF5 WITH_MPI
-build_and_test "On" "On" "Off"
-build_and_test "On" "Off" "Off"
-build_and_test "Off" "On" "Off"
-build_and_test "Off" "Off" "Off"
-build_and_test "Off" "Off" "On"
-
+build_and_test
