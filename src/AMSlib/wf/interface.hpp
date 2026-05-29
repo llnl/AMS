459808572b7778950ade5b102b9b92c1c9786ee5
Loic Pottier <pottier1@llnl.gov>
2026-05-29 16:07:04 -0700

    WIP. Removing Torch from the main path (all tests green with WITH_TORCH=On except integration tests)
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- src/AMSlib/wf/interface.hpp
+++ src/AMSlib/wf/interface.hpp
@@ -1,40 +1,32 @@
 #pragma once
-#include <c10/core/DeviceType.h>
-#include <torch/torch.h>
 
 #include "AMS.h"
+// #include "AMSTensor.hpp"
 
 namespace ams
 {
 class AMSWorkflow;
 }
 
+void callAMS(ams::AMSWorkflow *executor,
+             ams::DomainLambda Physics,
+             const ams::SmallVector<ams::AMSTensor> &ins,
+             ams::SmallVector<ams::AMSTensor> &inouts,
+             ams::SmallVector<ams::AMSTensor> &outs);
+
+
+#if defined(__AMS_ENABLE_TORCH__)
+
+#include <c10/core/DeviceType.h>
+#include <torch/torch.h>
+
 void callApplication(ams::DomainLambda CallBack,
                      ams::MutableArrayRef<torch::Tensor> Ins,
                      ams::MutableArrayRef<torch::Tensor> InOuts,
                      ams::MutableArrayRef<torch::Tensor> Outs);
 
-void callApplication(ams::HomogeneousGraphDomainFn CallBack,
-                     const ams::AMSHomogeneousGraph& graph,
-                     ams::AMSHomogeneousGraphFields& outputs);
-
-void callApplication(ams::HeterogeneousGraphDomainFn CallBack,
-                     const ams::AMSHeterogeneousGraph& graph,
-                     ams::AMSHeterogeneousGraphFields& outputs);
-
-
-void callAMS(ams::AMSWorkflow* executor,
-             ams::DomainLambda Physics,
-             const ams::SmallVector<ams::AMSTensor>& ins,
-             ams::SmallVector<ams::AMSTensor>& inouts,
-             ams::SmallVector<ams::AMSTensor>& outs);
-
-void callAMS(ams::AMSWorkflow* executor,
-             ams::HomogeneousGraphDomainFn Physics,
-             const ams::AMSHomogeneousGraph& graph_input,
-             ams::AMSHomogeneousGraphFields& outputs);
-
-void callAMS(ams::AMSWorkflow* executor,
-             ams::HeterogeneousGraphDomainFn Physics,
-             const ams::AMSHeterogeneousGraph& graph_input,
-             ams::AMSHeterogeneousGraphFields& outputs);
+/** @brief Helper to create AMSTensor views from a vector of torch::Tensors.
+*  @note The torch::Tensors MUST outlive the returned views.
+*/
+ams::SmallVector<ams::AMSTensor> torchToAMSTensors(ams::MutableArrayRef<torch::Tensor> tensorVector);
+#endif
