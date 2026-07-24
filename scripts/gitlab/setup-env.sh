46ed1ee4447fc063ca11201f158c433deb99d4a6
Loic Pottier <pottier1@llnl.gov>
2026-07-23 19:47:50 -0700

    Updated version that builds on Tuo
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- scripts/gitlab/setup-env.sh
+++ scripts/gitlab/setup-env.sh
@@ -16,11 +16,14 @@ SPACK_VER="1.1"
 
 if [[ "$SYS_TYPE" == "toss_4_x86_64_ib_cray" ]]; then
     echo "Loading MPI and ROCm"
-    module load gcc/13.3.1-magic
-    module load rocm/6.4.1
-    module load cray-mpich/8.1.32
+    module load rocmcc/6.4.3-magic
+    module load rocm/6.4.3
+    module load cray-mpich/9.0.1
+
     ROCM_ARCH=$(rocm_agent_enumerator | sed -n 1p)
     export CMAKE_CXX_FLAGS="-I${ROCM_PATH}/include/"
+    export CC=amdclang
+    export CXX=amdclang++
 elif [[ "$SYS_TYPE" == "toss_4_x86_64_ib" ]]; then
     # Dane
     module load cmake/3.30.5
@@ -67,12 +70,16 @@ export AMS_TORCH_PATH=$(echo $AMS_TORCH_PATH/lib*/python3.*/site-packages/torch/
 export AMS_AMQPCPP_PATH=$(echo $AMS_AMQPCPP_PATH/cmake)
 export AMS_CALIPER_PATH=$(echo $AMS_CALIPER_PATH/share/cmake/caliper)
 export AMS_NLOHMANN_JSON_DIR=$(echo $AMS_NLOHMANN_JSON_DIR/share/cmake/nlohmann_json/)
+export AMS_FMT_DIR=$(echo $AMS_FMT_DIR/lib*/cmake/fmt/)
 export AMS_TL_EXPECTED_DIR=$(echo $AMS_TL_EXPECTED_DIR/share/cmake/tl-expected/)
 
 echo "(for cmake) AMS_TORCH_PATH        = $AMS_TORCH_PATH"
 echo "(for cmake) AMS_AMQPCPP_PATH      = $AMS_AMQPCPP_PATH"
 echo "(for cmake) AMS_CALIPER_PATH      = $AMS_CALIPER_PATH"
 echo "(for cmake) AMS_NLOHMANN_JSON_DIR = $AMS_NLOHMANN_JSON_DIR"
+echo "(for cmake) AMS_FMT_DIR           = $AMS_FMT_DIR"
 echo "(for cmake) AMS_TL_EXPECTED_DIR   = $AMS_TL_EXPECTED_DIR"
+
+
 
 export AMS_LOG_LEVEL=Debug
